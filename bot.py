753efb02a01378326ef726a8d19e6e1ad56c6370
pxlys <aliff.kokol.1@gmail.com>
2026-04-07 21:43:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ from telegram.ext import (
 #   ⚙️  CONFIGURATION  — FILL THESE IN!
 # ─────────────────────────────────────────────
 
-
+import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
 
